ba1b80e787d6b29f2ffd87d611a5ea31dda521df
Anna Schmidt <marshall1697@gmail.com>
2022-10-06 12:35:03 -0600

    fullRecipe img style, added redirects file for deployment on netllify

--- src/components/FullRecipe.js
+++ src/components/FullRecipe.js
@@ -4,21 +4,16 @@ import { useParams } from 'react-router-dom';
 const FullRecipe = () => {
   const [fullRecipe, setFullRecipe] = useState(null);
   const params = useParams();
-  console.log(params)
-  
+
   useEffect(() => {
     fetch(`https://api.edamam.com/api/recipes/v2/${params.id}?type=public&app_id=${process.env.REACT_APP_API_ID}&app_key=${process.env.REACT_APP_API_KEY}
     `)
     .then(res => res.json())
     .then(data => {
-        console.log(data.recipe.label)
         setFullRecipe(data)
     })
     .catch(console.error("There was an error loading the recipe"))
   },[params])
-  
-console.log(fullRecipe)
-
 
 //for initial loading of data
 if (!fullRecipe) {
